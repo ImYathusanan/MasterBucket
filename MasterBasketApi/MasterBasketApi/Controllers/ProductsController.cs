e8ae1247be1b4d090cd8ff77bb4b20e49b17e334
Yathusanan <imyathusanan@gmail.com>
2020-07-01 02:44:02 -0700

    Initial Commit

---
+++ MasterBasketApi/MasterBasketApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasterBasketApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    //    [HttpGet]
+    //    public IActionResult GetProducts()
+    //    {
+    //        return "produts!";
+    //    }
+
+    //    [HttpGet("{id}")]
+    //    public IActionResult GetProduct(int id)
+    //    {
+    //        return "Product one";
+    //    }
+    }
+}
